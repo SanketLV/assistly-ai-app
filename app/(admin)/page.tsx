afaa5cfe8f7bf0b16a9760396fc3b4208240be22
SanketLV <sanketlakhani80@gmail.com>
2024-12-16 12:47:20 +0530

    add login, landing and loading page

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hello</h1>
+    <main className="p-10 bg-white m-10 rounded-md w-full">
+      <h1 className="text-4xl font-light">
+        Welcome to{" "}
+        <span className="text-[#64B5F5] font-semibold">Assistly</span>
+      </h1>
+
+      <h2 className="mt-2 mb-10">
+        Your customisable AI chat agent that helps you manage your customer
+        conversations.
+      </h2>
+
+      <Link href="/create-chatbot">
+        <Button className="bg-[#64B5F5] hover:bg-[#64B5F5] hover:opacity-50">
+          Lets get started by creating your first chatbot
+        </Button>
+      </Link>
     </main>
   );
 }
